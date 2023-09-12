c3da94ffd750cf5fcb769316996e76e82ba07801
Marcin Jaborski <marcin.jaborski@gmail.com>
2023-09-12 20:47:08 +0200

    Initial project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your </Text>
+      <Button title={"title"}/>
       <StatusBar style="auto" />
     </View>
   );
